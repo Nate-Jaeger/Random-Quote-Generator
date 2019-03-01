434c5234702c3144083bd9b9198209745466822a
Nate Jaeger <Nate.RJS21@gmail.com>
2019-02-28 19:35:50 -0600

    Added Tags property to quotes objects

--- js/script.js
+++ js/script.js
@@ -3,18 +3,22 @@ var quotes = [
   {
     quote: "Every accomplishment starts with the decision to try.",
     source: "John F. Kennedy",
+    tags: "Courage"
   },
   {
     quote: "A man is but a product of his thoughts. What he thinks, he becomes.",
     source: "Mahatma Gandhi",
+    tags: "Self-worth"
   },
   {
     quote: "There are no great limits to growth because there are no limits of human intelligence, imagination, and wonder.",
     source: "Ronald Reagan",
+    tags: "Inspiration"
   },
   {
     quote: "When something is important enough, you do it even if the odds are not in your favor.",
     source: "Elon Musk",
+    tags: "Bravery"
   },
   {
     quote: "They who dream by day are cognizant of many things which escape those who dream only by night.",
