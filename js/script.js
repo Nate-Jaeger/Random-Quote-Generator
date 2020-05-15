9363d635af81d71cb065adf5a4a321742134accb
Nate Jaeger <Nate.RJS21@gmail.com>
2020-05-14 21:31:31 -0500

    Moved quotes array to seperate file

--- js/script.js
+++ js/script.js
@@ -2,46 +2,6 @@
 //Information about setInterval read from: https://www.w3schools.com/js/js_timing.asp 
 const timer = window.setInterval(callFuncs, 20000);
 
-//Array of quote objects
-var quotes = [
-  {
-    quote: "Every accomplishment starts with the decision to try.",
-    source: "John F. Kennedy",
-    tags: "Courage"
-  },
-  {
-    quote: "A man is but a product of his thoughts. What he thinks, he becomes.",
-    source: "Mahatma Gandhi",
-    tags: "Self-worth"
-  },
-  {
-    quote: "There are no great limits to growth because there are no limits of human intelligence, imagination, and wonder.",
-    source: "Ronald Reagan",
-    tags: "Inspiration"
-  },
-  {
-    quote: "When something is important enough, you do it even if the odds are not in your favor.",
-    source: "Elon Musk",
-    tags: "Bravery"
-  },
-  {
-    quote: "They who dream by day are cognizant of many things which escape those who dream only by night.",
-    source: "Edgar Allan Poe",
-  },
-  {
-    quote: "It’s the possibility of having a dream come true that makes life interesting.",
-    source: "Paulo Coelho",
-    citation: "The Alchemist",
-    year: 1988,
-    tags: "Inspiration"
-  },
-  {
-    quote: "I don't know what frightens me more, the power that crushes us, or our endless ability to endure it.",
-    source: "Gregory David Roberts",
-    citation: "Shantaram",
-    year: 2003
-  },
-];
 
 //Function to create a random RGB value and post that value to background property
 function randomBG(){
