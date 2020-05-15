a3881271286b6f6a338a3d5811359a1841897747
Nate Jaeger <Nate.RJS21@gmail.com>
2020-05-14 21:36:07 -0500

    Replaced var with let/const

--- js/script.js
+++ js/script.js
@@ -5,27 +5,27 @@ const timer = window.setInterval(callFuncs, 20000);
 
 //Function to create a random RGB value and post that value to background property
 function randomBG(){
-  var x = Math.floor(Math.random() * 256);
-  var y = Math.floor(Math.random() * 256);
-  var z = Math.floor(Math.random() * 256);
-  var bgColor = `rgb(${x}, ${y}, ${z})`;
+  const x = Math.floor(Math.random() * 256);
+  const y = Math.floor(Math.random() * 256);
+  const z = Math.floor(Math.random() * 256);
+  const bgColor = `rgb(${x}, ${y}, ${z})`;
 
   document.body.style.background = bgColor;
 }
 
 function getRandomQuote() {
   //get a random number between 0 and quotes.length
-  var randomNumber = Math.floor(Math.random() * quotes.length);
+  const randomNumber = Math.floor(Math.random() * quotes.length);
 
   //Assign randomNumber to a random object in quotes array
-  var randomQuote = quotes[randomNumber];
+  const randomQuote = quotes[randomNumber];
   return randomQuote;
 }
 
 //Get randomQuote to print to screen
 function printQuote() {
-  var randomQuote = getRandomQuote();
-  var html = ``;
+  const randomQuote = getRandomQuote();
+  let html = ``;
 
   //Give html string the quote & source properties to display
   html += `<p class="quote">${randomQuote.quote} </p>`;
