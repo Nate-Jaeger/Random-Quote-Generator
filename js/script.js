135bd62ab83701270d7cec7c2d964f12a2afba08
Nate Jaeger <Nate.RJS21@gmail.com>
2020-05-14 21:13:49 -0500

    Added template literal to randomBG function

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ function randomBG(){
   var x = Math.floor(Math.random() * 256);
   var y = Math.floor(Math.random() * 256);
   var z = Math.floor(Math.random() * 256);
-  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  var bgColor = `rgb(${x}, ${y}, ${z})`;
 
   document.body.style.background = bgColor;
 }
